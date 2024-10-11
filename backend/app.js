c54aac78228dafbd3acd010dad96796b92719203
Stryker Huber <stryker.important@gmail.com>
2024-10-10 21:06:38 -0500

    started models, migrations, and seeders

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize')
 
 const { environment } = require('./config')
 const isProduction = environment === 'production'
@@ -47,6 +48,45 @@ const routes = require('./routes')
 
 
 app.use(routes)
+
+
+// Catch unhandled requests and forward to this error handler
+app.use((_req, _res, next) => {
+    const err = new Error("The Requested resource couldn't be found.")
+    err.title = "Resource Not Found"
+    err.errors = {
+        message: "The requested resource couldn't be found."
+    }
+    err.status = 404
+    next(err)
+})
+
+// Process sequelie errors
+app.use((err, _req, _res, next) => {
+    // Check if error is a Sequelize error
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of errors){
+            error[error.path] = error.message
+        }
+        err.title = 'Validation error'
+        err.errors = errors
+    }
+    next(err)
+})
+
+
+// Error formatting
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500)
+    console.error(err)
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    })
+})
 
 
 

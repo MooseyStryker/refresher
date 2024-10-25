ce56114ee6db6711b3f45e56c1dc8d548294119b
Stryker Huber <stryker.important@gmail.com>
2024-10-24 19:53:11 -0500

    Adding login criteria and checks in backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
+const sessionRouter = ('./session.js')
+const usersRouter = ('./users.js')
 const { restoreUser } = require('../../utils/auth.js')
 
 // This reaches our restoreUser middleware and checks the token to restore the user
 // If the current session is valid, restoreUser sets the req.user to the user in the db
 // If not, the req.user is then null
 router.use(restoreUser)
+
+router.use('/session', sessionRouter)
+
+router.use('/users', usersRouter)
+
+router.post('/test', (req, res) => {
+    res.json({
+        requestBody: req.body
+    })
+})
 
 
 module.exports = router
